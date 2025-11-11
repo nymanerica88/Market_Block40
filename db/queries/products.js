1bb2424a7b441099b98973258c4bf7f0c28709a4
Erica Nyman <ervin.erica29@gmail.com>
2025-11-11 18:20:47 -0500

    feat: added try/catch blocks & error handling

--- db/queries/products.js
+++ db/queries/products.js
@@ -6,16 +6,21 @@ import db from "#db/client";
 //{rows: [products]} takes the first row of the array and assigns it to the product
 //return products returns the new product (object) that was just created
 export async function createProduct(title, description, price) {
-  const sql = `
-    INSERT INTO products (title, description, price)
-    VALUES ($1, $2, $3)
-    RETURNING *
-    `;
+  try {
+    const sql = `
+      INSERT INTO products (title, description, price)
+      VALUES ($1, $2, $3)
+      RETURNING *
+      `;
 
-  const {
-    rows: [products],
-  } = await db.query(sql, [title, description, price]);
-  return products;
+    const {
+      rows: [products],
+    } = await db.query(sql, [title, description, price]);
+    return products;
+  } catch (error) {
+    console.error(`Error creating new product`, error);
+    throw error;
+  }
 }
 
 //getAllProducts - retrieves all products in the products table
@@ -24,27 +29,37 @@ export async function createProduct(title, description, price) {
 ////like in createProduct
 //return products returns the array of products from the table
 export async function getAllProducts() {
-  const sql = `
-    SELECT *
-    FROM products
-    `;
+  try {
+    const sql = `
+      SELECT *
+      FROM products
+      `;
 
-  const { rows: products } = await db.query(sql);
-  return products;
+    const { rows: products } = await db.query(sql);
+    return products;
+  } catch (error) {
+    console.error(`Error getting all products`);
+    throw error;
+  }
 }
 
 //getProductsById - retrieves product by id number
 //rows: [products]} since the product id is the primary key, it is unique, so this will only
 ////return a single object, so the same syntax that was used for createProduct is also used here
 export async function getProductById(id) {
-  const sql = `
-    SELECT *
-    FROM products
-    WHERE id = $1
-    `;
+  try {
+    const sql = `
+      SELECT *
+      FROM products
+      WHERE id = $1
+      `;
 
-  const {
-    rows: [products],
-  } = await db.query(sql, [id]);
-  return products;
+    const {
+      rows: [products],
+    } = await db.query(sql, [id]);
+    return products;
+  } catch (error) {
+    console.error(`Error Retrieving Product By Id`, error);
+    throw error;
+  }
 }
