2fa0b319680c97582712b10abe62ef24cae841cf
Erica Nyman <ervin.erica29@gmail.com>
2025-11-11 00:57:11 -0500

    feat: notated api/users.js for future reference; updated router to usersRouter

--- api/users.js
+++ api/users.js
@@ -1,11 +1,19 @@
 import express from "express";
-const router = express.Router();
-export default router;
+const usersRouter = express.Router();
+
+//import { createUser, getUserByUsernameAndPassword } - imports database query functions
+//import requireBody from "#middleware/requireBody"; - imports middleware function
+//import { createToken } from "#utils/jwt"; - imports helper function for the token
 
 import { createUser, getUserByUsernameAndPassword } from "#db/queries/users";
 import requireBody from "#middleware/requireBody";
 import { createToken } from "#utils/jwt";
 
+//POST (add) route at /register
+//requireBody ensures username and password exist in the request body
+//createUser inserts the new user into the database
+//createToken creates the JWT token containing the user id
+//sends 201 [Created] and returns the token
 router.post(
   "/register",
   requireBody(["username", "password"]),
@@ -17,6 +25,11 @@ router.post(
   }
 );
 
+//POST route at /login
+//again, requireBody ensures username and password exist in the request body
+//getUserByUsernameAndPassword retrieves the user if the user exists; user will be null if DNE
+//if authentication fails 401 [Unauthorized] and sends an error message
+//if authentication success, token is created and sent
 router.post(
   "/login",
   requireBody(["username", "password"]),
@@ -28,3 +41,5 @@ router.post(
     res.send(token);
   }
 );
+
+export default usersRouter;
