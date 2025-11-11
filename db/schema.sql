1a9b314fbec637755dd3654386900bddb26fd6cf
Erica Nyman <ervin.erica29@gmail.com>
2025-11-10 22:46:51 -0500

    feat: created tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,31 @@
--- TODO
+DROP TABLE orders_products;
+DROP TABLE orders;
+DROP TABLE products; 
+DROP TABLE users;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    date DATE NOT NULL,
+    note TEXT,
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE products(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    price NUMERIC(10, 2) NOT NULL
+);
+
+CREATE TABLE orders_products(
+    order_id INT NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+    product_id INT NOT NULL references products(id) ON DELETE CASCADE,
+    quantity INT NOT NULL,
+    PRIMARY KEY (order_id, product_id)
+);
