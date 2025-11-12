9e8d7119a17ee72fb9456678200f2e060a9b5a8d
Erica Nyman <ervin.erica29@gmail.com>
2025-11-12 01:16:16 -0500

    feat: added ordersRouter to app.js

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 import express from "express";
+import usersRouter from "./api/users.js";
+import productsRouter from "./api/products.js";
+import ordersRouter from "#api/orders";
+import getUserFromToken from "./middleware/getUserFromToken.js";
+
 const app = express();
+app.use(express.json());
+
+app.use(getUserFromToken);
+
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+app.use("/orders", ordersRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Error loading application" });
+});
+
 export default app;
