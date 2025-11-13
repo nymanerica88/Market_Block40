5676b39ba3428a55aaab6f2f537c4c9729e06c3f
Erica Nyman <ervin.erica29@gmail.com>
2025-11-13 17:03:13 -0500

    feat: notated file for reference; corrected typo

--- api/products.js
+++ api/products.js
@@ -5,6 +5,9 @@ import requireUser from "#middleware/requireUser";
 
 const productsRouter = express.Router();
 
+//GET route at /products; adds a GET route to productsRouter
+//Awaits the response from getAllProducts and then sends all products if successful
+//If unsuccuessful, it shows error message "Error retrieving product list" and moves to the next middleware
 productsRouter.get("/", async (req, res, next) => {
   try {
     const products = await getAllProducts();
@@ -15,6 +18,11 @@ productsRouter.get("/", async (req, res, next) => {
   }
 });
 
+//GET route at /products/:id; adds a GET route to productsRouter
+//const id - turns the id number into a number format
+//Awaits response from getProductById({id}) - passes the id in as an object
+//If there's an error retrieving product by id number, shows 404[Not Found] error and Product Not Found message
+//Sends the product if successful
 productsRouter.get("/:id", async (req, res, next) => {
   try {
     const id = Number(req.params.id);
@@ -27,6 +35,14 @@ productsRouter.get("/:id", async (req, res, next) => {
   }
 });
 
+//GET route at /products/:id/orders; adds a GET route to productsRouter
+//const product_id = Number(req.params.id) - takes the id from the URL and turns it into a number
+//const user_id = req.user.id - grabs the user_id
+//const product - awaits response from getProductById (with the product_id used as a parameter in the function)
+////if there is an error retrieving product, it returns a 404 error and a message "Product not found"
+//const orders - awaits response from getOrdersByProduct(with the product_id and user_id passed in as parameters)
+////if successful, shows a status of 200[OK], and sends the orders
+//if there's an error, it shows a message of "Error retrieving orders for product"
 productsRouter.get("/:id/orders", requireUser, async (req, res, next) => {
   try {
     const product_id = Number(req.params.id);
@@ -39,7 +55,7 @@ productsRouter.get("/:id/orders", requireUser, async (req, res, next) => {
 
     res.status(200).json(orders);
   } catch (error) {
-    console.error("Error retieving orders for product", error);
+    console.error("Error retrieving orders for product", error);
     next(error);
   }
 });
